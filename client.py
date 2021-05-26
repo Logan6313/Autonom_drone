01e1876fc4678f0fa55a9a2242260d8553824ea3
Logan6313 <robert.logan@orange.fr>
2021-05-26 16:28:42 +0300

    Add client.py

---
+++ client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import socket
+from time import sleep
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+# Connect the socket to the port where the server is listening
+server_address = ('192.168.1.86', 50010)
+print('Connecting to %s port %s' % server_address)
+sock.connect(server_address)
+ 
+
+# Send data
+message = raw_input ('->')
+enc_mes = message.encode()
+print('Sending "%s"' % message)
+sock.send(enc_mes)
+sleep(1)
+
+print('Closing socket')
+sock.close()
+
