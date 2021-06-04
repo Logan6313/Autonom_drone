541828c52995be2c848a969fe4bd9e628b47d1f7
Logan6313 <robert.logan@orange.fr>
2021-06-04 15:16:18 +0300

    Specific mission implemanted

--- Drone.py
+++ Drone.py
@@ -119,23 +119,28 @@ class Drone():
 
 		d=6371*math.acos(math.sin(c_lat)*math.sin(t_lat)+math.cos(c_lat)*math.cos(t_lat)*math.cos(t_lon-c_lon))
 		
-		return d
-		
-		
+		return d*1000
+
+
 	def mission(self):
 		print("Let's execute mission")
+		lat=[ -35.36424187, -35.36424187,-35.36424187]
+		lon=[ 149.16666566,149.16729521,149.16729521]
+		alt=[20,20,20]
 
 		cmds=self.pixhawk.commands
 		cmds.clear()
 		
 		takeoff=Command(0,0,0,mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,0,0,0,0,0,0,0,0,10)
 		speed=Command(0,0,0,mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED,0,0,0,15,-1,0,0,0,10)
-		point1=Command(0,0,0,mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,0, 0, 0, 0, 0, 0, -35.3649,149.16967,20)
-		rtl=Command(0,0,0,mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH,0,0,0,0,0,0,0,0,0)
+		point1=Command(0,0,0,mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,0, 0, 0, 0, 0, 0,lat[0],lon[0],alt[0])
+		point2=Command(0,0,0,mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,0, 0, 0, 0, 0, 0,lat[1],lon[1],alt[1])
+		point3=Command(0,0,0,mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,0, 0, 0, 0, 0, 0,lat[2],lon[2],alt[2])
 		cmds.add(takeoff)
 		cmds.add(speed)
 		cmds.add(point1)
-		cmds.add(rtl)
+		cmds.add(point2)
+		cmds.add(point3)
 		cmds.upload()
 
 		self.takeoff(10)
@@ -144,11 +149,18 @@ class Drone():
 		while True:
 			waypoint=self.pixhawk.commands.next
 			print("Current Waypoint: %s",waypoint)
+			if waypoint>1:
+				d=self.calc_distance(self.current_location(),LocationGlobalRelative(lat[waypoint-2],lon[waypoint-2],alt[waypoint-2]))
+				while d>10:
+					d=self.calc_distance(self.current_location(),LocationGlobalRelative(lat[waypoint-2],lon[waypoint-2],alt[waypoint-2]))	
+					print("Distance : " + str(d) + " meters")
+					sleep(1)
 			if waypoint==cmds.count:
 				break
 			sleep(1)
 
-		print("Sort de la boucle")
+		print("Return to Launch")
+		self.set_mode("RTL")
 		
 
 if __name__=="__main__":
