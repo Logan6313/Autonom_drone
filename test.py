9f855b772d655bb970ec7e0582780e3cf88de61b
Logan6313 <robert.logan@orange.fr>
2021-07-09 15:54:41 +0300

    Add method to change airspeed + textEditor to receive message from NVIDIA

--- test.py
+++ test.py
@@ -9,9 +9,8 @@ import GUI
 class Window(QtGui.QMainWindow,GUI.Ui_MainWindow):  
 	def __init__(self,parent=None):
 
-		super(Window, self).__init__(parent)   #call the constructor QtGui.QWidget
+		super(Window, self).__init__(parent)  
 		self.setupUi(self)
-		
 		self.commandLinkButton.clicked.connect(self.button)
 		self.pushButton_2.clicked.connect(self.close_socket)
 		self.comboBox.currentIndexChanged.connect(self.list_event)
@@ -20,8 +19,6 @@ class Window(QtGui.QMainWindow,GUI.Ui_MainWindow):
 		self.radioButton.clicked.connect(self.arm_on)
 		self.radioButton_2.clicked.connect(self.arm_off)
 		self.pushButton.clicked.connect(self.mission)
-
-
 
 
 	def list_event(self):
@@ -51,13 +48,17 @@ class Window(QtGui.QMainWindow,GUI.Ui_MainWindow):
 			self.label_11.setText("1 parameter :")
 
 		elif rank==10:
+			self.label_2.setText("Airspeed (m/s)")
+			self.label_11.setText("1 parameter :")
+
+		elif rank==11:
 			self.label_2.setText("Vel_x (m/s)")
 			self.label_3.setText("Vel_y (m/s)")
 			self.label_4.setText("Vel_z (m/s)")
 			self.label_5.setText("Duration (s)")
 			self.label_11.setText("4 parameters :")
 
-		elif rank==11:
+		elif rank==12:
 			self.label_2.setText("Lat (degree)")
 			self.label_3.setText("Lon (degree)")
 			self.label_4.setText("Alt (m)")
@@ -96,7 +97,7 @@ class Window(QtGui.QMainWindow,GUI.Ui_MainWindow):
 		elif self.comboBox.currentIndex()==3:
 			mes=str(self.comboBox.currentText() + " " + self.lineEdit_2.text()+ " " + self.lineEdit_3.text()+ " " + self.lineEdit_4.text())
 			
-		elif self.comboBox.currentIndex()>4 and self.comboBox.currentIndex()<10:
+		elif self.comboBox.currentIndex()>4 and self.comboBox.currentIndex()<11:
 			mes=str(self.comboBox.currentText() + " " + self.lineEdit_2.text())
 			
 		else:
@@ -132,7 +133,7 @@ class Window(QtGui.QMainWindow,GUI.Ui_MainWindow):
 		sock.send(enc_mes)
 		data=sock.recv(4096)
 		print(data)
-		self.lineEdit.setText(data)
+		self.textBrowser.setText(data)
 
 	def clear(self):
 		print("clear")
@@ -149,13 +150,13 @@ class Window(QtGui.QMainWindow,GUI.Ui_MainWindow):
 
 if __name__ == "__main__":
 
-	# Create a TCP/IP socket
+	"""# Create a TCP/IP socket
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	 
 	# Connect the socket to the port where the server is listening
 	server_address = ('192.168.1.86', 50010)
 	print('Connecting to %s port %s' % server_address)
-	sock.connect(server_address)
+	sock.connect(server_address)"""
 
 	app = QtGui.QApplication(sys.argv)
 	win = Window()
